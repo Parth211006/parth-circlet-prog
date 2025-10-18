50dc65100f905122b6d2250970bcd4817a11c2c1
PayalPatel03 <p6579113@gmail.com>
2025-10-18 15:26:36 +0530

    update

---
+++ prog4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    for (int i = 5; i >= 1; i--)
+    {
+        for (int k = 1; k <= 5- i; k++)
+        {
+            printf("  ");
+        }
+        for (int j = 1; j<= i; j++)
+        {
+            if (j%2==0)
+            {
+                printf("%d ",0);
+            }
+            else
+            {
+                printf("%d ", 1);
+            }
+            
+        }
+        printf("\n");
+    }
+}
